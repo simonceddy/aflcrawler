7a26c6321e2180d731d081cb7d711addb1910981
Simon Eddy <simon@simoneddy.com.au>
2019-04-03 22:27:14 +1100

    added placeholder group for eloquent factories

--- config/afl/factories.php
+++ config/afl/factories.php
@@ -2,11 +2,25 @@
 return [
     'use' => 'basic',
     'groups' => [
+        
+        /**
+         * Default factories - requires no extra dependencies
+         */
         'basic' => [
             'player' => AflCrawler\Factory\Basic\PlayerFactory::class,
             'team' => AflCrawler\Factory\Basic\TeamFactory::class,
             'roster' => AflCrawler\Factory\Basic\RosterFactory::class,
             'rostered-player' => AflCrawler\Factory\Basic\RosteredPlayerFactory::class,
+        ],
+
+        /**
+         * Laravel/Eloquent factories
+         */
+        'eloquent' => [
+            'player' => '',
+            'team' => '',
+            'roster' => '',
+            'rostered-player' => ''
         ]
     ]
 ];
